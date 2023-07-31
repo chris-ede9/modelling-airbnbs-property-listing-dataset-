230ad992c3ea7ac223095451017c1c621d5b558b
chris-ede9 <chris_ede9@outlook.com>
2023-07-31 18:24:18 +0100

    Completed the Data Preparation Milestone

--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,15 @@
 # Modelling Airbnb's property listing dataset
 
- This solution will build a framework to systematically train, tune, and evaluate models on several tasks that are tackled by the Airbnb team
+ This solution will build a framework to systematically train, tune, and evaluate models on several tasks that are tackled by the Airbnb team.
+
+ ## Project Milestones for the Project:
+
+### Milestone 1 - Data Preparation
+
+> Before building the framework, the first goal was to understand how the Airbnb dataset is structured and clean it accordingly.
+
+>> The source of the data is stored in listing.csv and the code to cleanse the data to get it ready to be analysed is in tabular_data.py
+
+- Task 1: Loaded in the tabular dataset and functions written that cleanse the ratings, description and guest data fields.
+
+- Task 2: Created a Pandas dataframe consisting of features that contain numerical data only and set the label as the "Price_night" as this will be what is determined in the model that will be created.
---
+++ tabular_data.py
@@ -0,0 +1,96 @@
+import pandas as pd
+
+def remove_rows_with_missing_ratings(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    This method removes all rows in a DataFrame that contain NA values in the Ratings columns
+
+    df: Pandas Dataframe - The Dataframe that is going to be cleaned
+
+    returns: Pandas Dataframe - DataFrame with cleaned rows containing a rating value
+    '''
+
+    # Drop records with missing ratings
+    df = df.dropna(subset=['Cleanliness_rating'])
+    # In example all rows the same with missing ratings, but just in case check all ratings columns
+    df = df.dropna(subset=['Accuracy_rating'])
+    df = df.dropna(subset=['Location_rating'])
+    df = df.dropna(subset=['Check-in_rating'])
+    df = df.dropna(subset=['Value_rating'])
+
+    return df
+
+def combine_description_strings(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    This method removes all rows in a DataFrame that contain no description and converts the description
+    into a single string rather than a list of strings
+
+    df: Pandas Dataframe - The Dataframe that contains the Description field that will be cleaned
+
+    returns: Pandas Dataframe - DataFrame with a cleaned up Description field
+    '''
+
+     # Drop records with missing descriptions
+    df.dropna(subset=['Description'], inplace=True)
+
+    # Clean the description to remove the "About this space" prefix and remove any whitespace
+    df['Description'] = df['Description'].str.replace(r'About this space', '', regex=True).str.strip()
+    df['Description'] = df['Description'].str.replace(r'^\s*\'\',*\s*', '', regex=True).str.strip()
+    df['Description'] = df['Description'].str.replace(r'[\[\],\']', '', regex=True).str.strip()
+
+    return df
+
+def set_default_feature_values(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    This method replaces any empty values to 1 for the following columns:
+    guests, beds, bathrooms, bedrooms
+
+    df: Pandas Dataframe - The Dataframe that will update the empty values to 1
+
+    returns: Pandas Dataframe - DataFrame with default values applied
+    '''
+    
+    default_values = {'guests': 1, 'beds': 1, 'bathrooms': 1, 'bedrooms': 1}
+    df.fillna(default_values, inplace=True)
+    # Drop bad column from dataset as not required
+    df.drop(columns=['Unnamed: 19'], inplace=True)
+
+    return df
+
+def clean_tabular_data(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    This method calls all the assocaited methods to clean the dataframe
+
+    df: Pandas Dataframe - The Dataframe that is to be cleaned
+
+    returns: Pandas Dataframe - DataFrame with cleaned up data
+    '''
+
+    df = remove_rows_with_missing_ratings(df)
+    df = combine_description_strings(df)
+    df = set_default_feature_values(df)
+
+    return df
+
+def load_airbnb(df: pd.DataFrame, label: str) -> tuple:
+    '''
+    This method gets the list of features and labels of the data
+
+    df: Pandas Dataframe - The Dataframe that contains the features and labels
+    label: str - The name of the column that will be the label
+
+    returns: tuple - A tuple of features and labels
+    '''
+
+    features = df.drop(columns=[label])
+    labels = df[label]
+
+    # Filter out columns containing text data for the features
+    features = features.select_dtypes(include='number')
+
+    return features, labels
+
+if __name__ == "__main__":
+    airbnb_df = pd.read_csv('listing.csv', delimiter=',')
+    airbnb_df = clean_tabular_data(airbnb_df)
+    features, labels = load_airbnb(airbnb_df, 'Price_Night')
+    
